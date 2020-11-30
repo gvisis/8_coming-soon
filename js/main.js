6fc0090435e35ae87989ae3c2ae1d60f0fdcc866
gvisis <gvisis@gmail.com>
2020-11-30 17:31:13 +0200

    Single progress bar validation and some JSDoc

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ renderAllProgressBars(progressBarData);
 //----------------------
 // skirtingose vietose '.main > .row' ir footer.row generuos ta pati turini is socialsData
 // renderSocials('main > .row', socialsData2);
+
 //------------------------
 /* const h1DOM = document.querySelector('h1'); // randa viena ir pirma elementa su tagu h1;; querySelector yra vienas elementas
 const inputDOM = document.querySelectorAll('input'); // suras visus inputus .form klaseje
